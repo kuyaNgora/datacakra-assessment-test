3a21684522a7bd1042344aae0f43e8868f612dd0
Badar Amri Suri <badaramrisuri@gmail.com>
2024-12-09 07:58:16 +0700

    fix warning

--- src/services/store.tsx
+++ src/services/store.tsx
@@ -10,7 +10,7 @@
  */
 
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer, WebStorage } from "redux-persist";
+import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import rootReducer from "./reducers";
 
